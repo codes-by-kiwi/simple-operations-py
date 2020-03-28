83914533929030e2a8717caed8ffbe737090fcbe
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-03-28 12:23:22 +0100

    Add files via upload

---
+++ simpop.py
@@ -0,0 +1,14 @@
+
+three = 3
+four = 4
+seven = three + four
+print(seven)
+
+#concatenating strings 
+welcome = "welcome"
+to = "to"
+coding = "coding"
+welcometocoding = welcome + " " + to + " " + coding
+print(welcometocoding)
+
+#this shows simple operations on number and strings
